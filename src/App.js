1ab919479c47987620e47a830c117cfedf59c03d
Sam Bao <sam.b5687@gmail.com>
2024-02-01 19:03:22 -0700

    some basic UI clean up

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@
 import "./App.css";
 import Input from "./Input.js";
 import Map from "./map";
-import { useState, useRef } from "react";
+import { useState, useRef, useLayoutEffect } from "react";
 import { useLoadScript } from "@react-google-maps/api";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Grid,Stack } from '@mui/material';
 import YourFavoriteSpotifyArtists from "./YourFavoriteSpotifyArtists.js";
 import PickDate from "./PickDate.js";
+
+function useWindowSize() {
+  const [size, setSize] = useState([0, 0]);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+}
 
 function App() {
   let cachedStartDate = localStorage.getItem('startDate');
   let cachedEndDate = localStorage.getItem('endDate');
+  const [width, height] = useWindowSize();
   const [concerts, setConcerts] = useState([]);
   const [userLocation, setUserLocation] = useState(null);
   const [startDate, setStartDate] = useState(cachedStartDate === null ? new Date() : new Date(cachedStartDate));
@@ -28,20 +43,43 @@ function App() {
   };
   return (
     <div className="App">
-      <header className="App-header">
-        <PickDate updateStartDateInParent={setStartDate} updateEndDateInParent={setEndDate} />
-        <Input setConcerts={setConcerts} setUserLocation={setUserLocation} setMapStyle={setMapStyle} startDate={startDate} endDate={endDate} ref={childRef} />
-        <Router>
-          <Routes>
-            <Route
-              path="/"
-              element={<YourFavoriteSpotifyArtists onChildClick={handleChildClick} startDate={startDate} endDate={endDate}></YourFavoriteSpotifyArtists>}
-            />
-          </Routes>
-        </Router>
-      </header>
-
-      {isLoaded ? <Map concerts={concerts} userLocation={userLocation} mapStyle={mapStyle} /> : null}
+      {width > 700 ? (
+        <Grid className="App-header" container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <PickDate updateStartDateInParent={setStartDate} updateEndDateInParent={setEndDate} />
+            <Input setConcerts={setConcerts} setUserLocation={setUserLocation} setMapStyle={setMapStyle} startDate={startDate} endDate={endDate} ref={childRef} />
+            <Router>
+              <Routes>
+                <Route
+                  path="/"
+                  element={<YourFavoriteSpotifyArtists onChildClick={handleChildClick} startDate={startDate} endDate={endDate}></YourFavoriteSpotifyArtists>}
+                />
+              </Routes>
+            </Router>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            {isLoaded ? <Map concerts={concerts} userLocation={userLocation} mapStyle={mapStyle} /> : null}
+          </Grid>
+        </Grid>
+      ) : (
+        <Stack spacing={2}>
+          <Grid container item xs={12} md={6} sx={{ display: { xs: 'none', md: 'flex' } }}>
+            <PickDate updateStartDateInParent={setStartDate} updateEndDateInParent={setEndDate} />
+            <Input setConcerts={setConcerts} setUserLocation={setUserLocation} setMapStyle={setMapStyle} startDate={startDate} endDate={endDate} ref={childRef} />
+            <Router>
+              <Routes>
+                <Route
+                  path="/"
+                  element={<YourFavoriteSpotifyArtists onChildClick={handleChildClick} startDate={startDate} endDate={endDate}></YourFavoriteSpotifyArtists>}
+                />
+              </Routes>
+            </Router>
+          </Grid>
+          <Grid container item xs={12} md={6} sx={{ display: { xs: 'none', md: 'flex' } }}>
+            {isLoaded ? <Map concerts={concerts} userLocation={userLocation} mapStyle={mapStyle} /> : null}
+          </Grid>
+        </Stack>
+      )}
     </div>
   );
 }
