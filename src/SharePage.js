e49a7a84ae1fef53eee1900d387d2f26921a74e3
Sam Bao <sam.b5687@gmail.com>
2024-02-06 23:36:05 -0700

    splitting concert list into two columns in the sharepage component

--- src/SharePage.js
+++ src/SharePage.js
@@ -22,46 +22,87 @@ const SharePage = ({ concerts, userLocation, mapStyle }) => {
         googleMapsApiKey: process.env.REACT_APP_GCP_KEY, // Add your API key
     });
 
+    const middleIndex = Math.ceil(concerts.length / 2);
+    const concerts1 = concerts.slice(0,middleIndex);
+    const concerts2 = concerts.slice(middleIndex);
 
-    const renderConcertList = concerts.map((concert, index) => {
-        return (
-            <Grid item xs={6} sm={6} md={6} key={index}>
-                <ListItem
-                    key={index}
-                    disablePadding
-                >
-                    <ListItemButton>
-                        <ListItemAvatar>
-                            <Avatar
-                                alt={`${concert.artist}`}
-                                src={`${concert.image.url}`}
-                            />
-                        </ListItemAvatar>
-                        <ListItemText
-                            primary={`${concert.artist}`}
-                            secondary={
-                                <React.Fragment>
-                                    <Stack direction="row" spacing={1}>
-                                        <Chip color="primary" label={`${concert.location.name}`} />
-                                        <Chip color="primary" label={`${formattedDate(concert.date)}`} />
-                                    </Stack>
-                                </React.Fragment>
-                            } />
-                    </ListItemButton>
-                </ListItem>
-            </Grid>
-        );
-    });
 
-    return (
-        <Stack spacing={2} alignItems={'flex-start'} justifyContent={'flex-start'}>
-            <div>{isLoaded ? <Map concerts={concerts} userLocation={userLocation} mapStyle={mapStyle} /> : null}</div>
-            <div><List
-                dense>
-                <Grid container spacing={{ xs: 2, md: 3 }} >
-                    {renderConcertList}
+    const renderConcertList1 =
+        concerts1.map((concert, index) => {
+            return (
+                <Grid item  key={index}>
+                    <ListItem
+                        key={index}
+                        disablePadding
+    
+                    >
+                        <ListItemButton>
+                            <ListItemAvatar>
+                                <Avatar
+                                    alt={`${concert.artist}`}
+                                    src={`${concert.image.url}`}
+                                />
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary={`${concert.artist}`}
+                                secondary={
+                                    <React.Fragment>
+                                        <Stack direction="row" spacing={1}>
+                                            <Chip color="primary" label={`${concert.location.name}`} />
+                                            <Chip color="primary" label={`${formattedDate(concert.date)}`} />
+                                        </Stack>
+                                    </React.Fragment>
+                                } />
+                        </ListItemButton>
+                    </ListItem>
                 </Grid>
-            </List></div>
+            );
+        });
+    const renderConcertList2 = 
+        concerts2.map((concert, index) => {
+            return (
+                <Grid item xs={12} sm={12} md={12} key={index}>
+                    <ListItem
+                        key={index}
+                        disablePadding
+    
+                    >
+                        <ListItemButton>
+                            <ListItemAvatar>
+                                <Avatar
+                                    alt={`${concert.artist}`}
+                                    src={`${concert.image.url}`}
+                                />
+                            </ListItemAvatar>
+                            <ListItemText
+                                primary={`${concert.artist}`}
+                                secondary={
+                                    <React.Fragment>
+                                        <Stack direction="row" spacing={1}>
+                                            <Chip color="primary" label={`${concert.location.name}`} />
+                                            <Chip color="primary" label={`${formattedDate(concert.date)}`} />
+                                        </Stack>
+                                    </React.Fragment>
+                                } />
+                        </ListItemButton>
+                    </ListItem>
+                </Grid>
+            );
+        });
+    
+    return (
+        <Stack spacing={2} >
+            {isLoaded ? <Map concerts={concerts} userLocation={userLocation} mapStyle={mapStyle} /> : null}
+
+            <Grid container spacing={{ xs: 2, md: 3 }} direction={'row'}>
+                <List dense>
+                    {renderConcertList1}
+                </List>
+                <List dense>
+                    {renderConcertList2}
+                </List>
+            </Grid>
+
         </Stack>
     );
 };
