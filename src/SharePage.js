4bda9865f8975594adef3909b06593f64ed8aa89
Sam Bao <sam.b5687@gmail.com>
2024-02-07 22:05:48 -0700

    adding cool trip title to share page

--- src/SharePage.js
+++ src/SharePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Stack } from '@mui/material';
+import { Stack, TextField } from '@mui/material';
 import Map from "./map";
 import { useLoadScript } from "@react-google-maps/api"
 import SharePageList from './SharePageList';
@@ -11,7 +11,7 @@ const SharePage = ({ concerts, userLocation, mapStyle }) => {
     });
 
     const middleIndex = Math.ceil(concerts.length / 2);
-    const concerts1 = concerts.slice(0,middleIndex);
+    const concerts1 = concerts.slice(0, middleIndex);
     const concerts2 = concerts.slice(middleIndex);
 
     return (
@@ -19,7 +19,7 @@ const SharePage = ({ concerts, userLocation, mapStyle }) => {
             <Stack container xs={6} sm={6} md={6}>
                 {isLoaded ? <Map concerts={concerts} userLocation={userLocation} mapStyle={mapStyle} /> : null}
             </Stack>
-
+            <TextField id="standard-basic" variant="standard" placeholder="Write a cool name for your trip here"  InputProps={{ sx: { '& input': { textAlign: 'center', color:'white' } } }}/>
             <Stack justifyContent="space-evenly" xs={12} sm={12} md={12} container spacing={{ xs: 2, md: 3 }} direction={'row'}>
                 <SharePageList concerts={concerts1} />
                 <SharePageList concerts={concerts2} />
