c9459a5bcff4bcc6175c64be05985c6946c5065b
Sam Bao <sam.b5687@gmail.com>
2024-02-08 21:59:06 -0700

    removing html2canvas since it does a terrible job of rendering

--- src/SharePage.js
+++ src/SharePage.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Stack, TextField, Button } from '@mui/material';
+import { Stack, TextField } from '@mui/material';
 import Map from "./map";
 import { useLoadScript } from "@react-google-maps/api"
 import SharePageList from './SharePageList';
-import html2canvas from 'html2canvas';
 
 const SharePage = ({ concerts, userLocation, mapStyle }) => {
     // Your component logic goes here
@@ -15,24 +14,9 @@ const SharePage = ({ concerts, userLocation, mapStyle }) => {
     const concerts1 = concerts.slice(0, middleIndex);
     const concerts2 = concerts.slice(middleIndex);
 
-    const handleDownloadImage = async () => {
-        const element = document.getElementById('SharePage'),
-        canvas = await html2canvas(element),
-        data = canvas.toDataURL('image/jpg'),
-        link = document.createElement('a');
-    
-        link.href = data;
-        link.download = 'downloaded-image.jpg';
-    
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      };
-
     return (
-        <Stack sx={{ width: '100wh' }} spacing={2}  >
-            <div id="SharePage">
-                <Stack >
+        <Stack sx={{ width: '100wh' }} spacing={2} >
+           <Stack container xs={6} sm={6} md={6}>
                     {isLoaded ? <Map concerts={concerts} userLocation={userLocation} mapStyle={mapStyle} /> : null}
                 </Stack>
                 <TextField variant="standard" placeholder="Write a cool name for your trip here" InputProps={{ sx: { '& input': { textAlign: 'center', color: 'white' } } }} />
@@ -40,8 +24,6 @@ const SharePage = ({ concerts, userLocation, mapStyle }) => {
                     <SharePageList concerts={concerts1} />
                     <SharePageList concerts={concerts2} />
                 </Stack>
-            </div>
-                <Button onClick={handleDownloadImage} variant="contained">Share As Image</Button>
         </Stack>
     );
 };
