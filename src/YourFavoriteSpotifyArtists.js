e2f6c8815f6326e6853ce799884f902f082217be
Sam Bao <sam.b5687@gmail.com>
2024-03-01 18:01:17 -0700

    basic front end to triggering playlist artist show search

--- src/YourFavoriteSpotifyArtists.js
+++ src/YourFavoriteSpotifyArtists.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Grid, Chip, Button, Box } from '@mui/material';
+import { Grid, Chip, Button, Box, Stack, TextField } from '@mui/material';
 import { useLocation } from "react-router-dom";
 
 function YourFavoriteSpotifyArtists({ onChildClick, startDate, endDate }) {
@@ -9,7 +9,7 @@ function YourFavoriteSpotifyArtists({ onChildClick, startDate, endDate }) {
   const [codeVerifier, setCodeVerifier] = useState("");
   const [followedArtists, setFollowedArtists] = useState([]);
   const [disableButton, setDisableButton] = useState(false);
-
+  const [spotifyPlayList, setSpotifyPlaylist] = useState("");
   const handleClick = (artistName) => {
     onChildClick(artistName);
   };
@@ -17,15 +17,11 @@ function YourFavoriteSpotifyArtists({ onChildClick, startDate, endDate }) {
 
   const commaSeparatedfollowedArtists = followedArtists.map((artistName, index) => {
     return (
-
       <Grid item key={index}>
         <Chip sx={{ background: "limegreen" }} label={artistName} color="success" onClick={() => handleClick(artistName)} />
       </Grid>
-
     );
   });
-
-
 
   useEffect(() => {
     async function fetchAccessData() {
@@ -68,6 +64,41 @@ function YourFavoriteSpotifyArtists({ onChildClick, startDate, endDate }) {
     }
   }, [code, codeVerifier])
 
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const url = spotifyPlayList;
+    const startIndex = url.lastIndexOf("/") + 1; // Find the index of the last slash and add 1
+    const endIndex = url.indexOf("?"); // Find the index of the question mark
+
+    const extractedPlaylistId = url.substring(startIndex, endIndex);
+
+    await fetch(`${process.env.REACT_APP_BACKEND}/FindArtistWithShows/GetSpotifyPlaylistArtistsWithShows`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify({
+        playlistId: extractedPlaylistId,
+        startDate: startDate,
+        endDate: endDate
+      }),
+    }).then(async (res) => {
+      console.log(`response status code: ${res.status}`);
+      if (res.status === 200) {
+        let resJson = await res.json();
+        const existingArtists = followedArtists;
+        const updatedArtists = [...existingArtists, ...resJson].filter(
+          (value, index, self) => self.indexOf(value) === index
+        );
+        setFollowedArtists(updatedArtists);
+      }
+      return;
+    }).catch((err) => {
+      console.log("Some error occured");
+      console.log(err);
+    });
+  };
   let getSpotifyArtist = async () => {
     //get access token 
     var access_token = localStorage.getItem("access_token");
@@ -108,8 +139,40 @@ function YourFavoriteSpotifyArtists({ onChildClick, startDate, endDate }) {
   // Display spotify token 
   return (
     <Grid>
-      <p>Top Artists: </p>
-      {disableButton ? (
+      <p>Upcoming Artists With Shows: </p>
+      <form onSubmit={handleSubmit}>
+        <Stack direction={'column'} spacing={2}>
+          <TextField
+            sx={{
+              "& input": {
+                color: "white",
+              },
+              "& label": {
+                color: "white",
+              },
+            }}
+            label="Spotify Playlist URL"
+            value={spotifyPlayList} onChange={(e) => setSpotifyPlaylist(e.target.value)}
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+          >
+            Submit
+          </Button>
+        </Stack>
+      </form>
+      
+      {disableButton ? (<div></div>
+) : (
+        <Button
+          sx={{ background: "limegreen" }}
+          variant="contained"
+          color="success"
+          onClick={getSpotifyArtist}>
+          Get your top artists
+        </Button>)}
         <Box
           display="flex"
           justifyContent="center"
@@ -119,14 +182,7 @@ function YourFavoriteSpotifyArtists({ onChildClick, startDate, endDate }) {
           <Grid xs={10} md={10} container spacing={1} direction="row" justifyContent="center">
             {commaSeparatedfollowedArtists}
           </Grid>
-        </Box>) : (
-        <Button
-          sx={{ background: "limegreen" }}
-          variant="contained"
-          color="success"
-          onClick={getSpotifyArtist}>
-          Get your top artists
-        </Button>)}
+        </Box>
     </Grid>
   );
 }
