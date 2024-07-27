07fb5286cd85a44fc8c84417ba5ac4bfa5b1242b
Sam Bao <sam.b5687@gmail.com>
2024-07-27 14:12:05 -0600

    add gig count to artist choice list, so people know whether somebody have upcoming concert

--- src/ArtistChoiceList.js
+++ src/ArtistChoiceList.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import { List, ListItem, ListItemButton, ListItemAvatar, ListItemText, Avatar } from '@mui/material';
 
-const ArtistChoiceList = ({ artists, onArtistClick}) => {
-    return (
-        <List>
-            {artists.map((artist, index) => (
-                <ListItem key={index}>
-                <ListItemButton onClick={() => onArtistClick(artist)}>
-                  <ListItemAvatar>
-                    <Avatar                      
-                      alt={`${artist.name}`}
-                      src={`${artist.images[1].url}`}
-                    />
-                  </ListItemAvatar>
-                  <ListItemText
-                                primary={`${artist.name}`}
-                                ></ListItemText>
-                </ListItemButton>
-              </ListItem>
-            ))}
-        </List>
-    );
+const ArtistChoiceList = ({ artists, onArtistClick }) => {
+  const text = { color: 'red' }
+  return (
+    <List>
+      {artists.map((artist, index) => (
+        <ListItem key={index}>
+          <ListItemButton onClick={() => onArtistClick(artist)}>
+            <ListItemAvatar>
+              <Avatar
+                alt={`${artist.name}`}
+                src={`${artist.images[1].url}`}
+              />
+            </ListItemAvatar>
+            <ListItemText
+
+              primary={`${artist.name}`}
+              secondary={`${artist.gigCount} Gig(s)`}
+              secondaryTypographyProps={{ style: text }}
+            ></ListItemText>
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
+  );
 };
 
 export default ArtistChoiceList;
-    
