4de23f15298938efdcb531ee138be0e0bf03e802
Sam Bao <sam.b5687@gmail.com>
2024-12-04 22:52:15 -0700

    dropping OpenAi package from front end

--- src/Odyssey/FetchName.js
+++ src/Odyssey/FetchName.js
@@ -1,31 +1,19 @@
-const { AzureOpenAI } = require("openai");
-// You will need to set these environment variables or edit the following values
-const endpoint = process.env.REACT_APP_AZURE_OPENAI_ENDPOINT;
-const apiKey = process.env.REACT_APP_AZURE_OPENAI_API_KEY;
-const instructions =  process.env.REACT_APP_AI_INSTRUCTIONS; 
-const apiVersion = "2024-08-01-preview";
-const deployment = "gpt-4o-mini"; //This must match your deployment name.
-
-export const FetchName = async (concerts) => {
-    const client = new AzureOpenAI({ endpoint, apiKey, apiVersion, deployment });
-
-    let inputstring = concerts.map(concert => JSON.stringify(concert)).join(', ');
-    const result = await client.chat.completions.create({
-        response_format:{ "type": "json_object" },
-        messages: [
-            { role: "system", content: instructions },
-            { role: "user", content: inputstring },
-        ],
-        model: "",
-    }).catch((err) => {
-        console.error("The sample encountered an error:", err);
-    });
-    ;
-
-    for (const choice of result.choices) {
-        console.log(choice.title);
-    }
-
-    return result;
-
+export const FetchName = async (concerts, setPosterName, setPosterNameSuggestions) => {    
+    const response = await fetch(
+        `${process.env.REACT_APP_BACKEND}/GetTripTitleSuggestion`,
+        {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(concerts),
+        }
+      );
+      console.log(`response status code: ${response.status}`);
+      if (response.status === 200) {
+        let resJson = await response.json();        
+        setPosterName(resJson[0]);
+        setPosterNameSuggestions(resJson);
+      }
+      return;
 };
