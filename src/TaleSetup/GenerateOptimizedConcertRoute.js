128e2364c50cb1b5fea12bf1aeb7c7839ddc1892
Sam Bao <sam.b5687@gmail.com>
2025-05-20 23:04:57 -0600

    concert routing optimization

--- src/TaleSetup/GenerateOptimizedConcertRoute.js
+++ src/TaleSetup/GenerateOptimizedConcertRoute.js
@@ -51,83 +51,86 @@ function distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2) {
   return earthRadiusKm * c;
 }
 
-
-
 function generateCombinations(dictionary) {
   const keys = Object.keys(dictionary);
   const result = [];
 
-  function backtrack(combination, index, lastDate, lastLocation, lastTitle) {
+  function backtrack(combination, index, lastDate, lastLocation, lastTitle, filteredDictionary) {
     if (index === keys.length) {
       result.push([...combination]);
       return;
     }
 
     const key = keys[index];
-    const values = dictionary[key];
 
-    for (const value of values) {
+    // **Filter dictionary BEFORE the loop** to reduce unnecessary checks
+    filteredDictionary[key] = filteredDictionary[key].filter(value => new Date(value.date) >= lastDate);
+
+    for (const value of filteredDictionary[key]) {
       const concertDate = new Date(value.date);
       if (concertDate >= lastDate && (concertDate !== lastDate || (value.location.name === lastLocation.name && value.location.address === lastLocation.address && value.title === lastTitle))) {
         combination.push(value);
-        backtrack(combination, index + 1, concertDate, value.location, value.title);
+        // Pass the already filtered dictionary forward
+        backtrack(combination, index + 1, concertDate, value.location, value.title, filteredDictionary);
         combination.pop();
       }
     }
   }
 
-  backtrack([], 0, new Date(0), { name: null, address: null }, null);
+  backtrack([], 0, new Date(0), { name: null, address: null }, null, { ...dictionary });
   return result;
 }
 
+
+
 export const GenerateOptimizedConcertRoute = (
-    allConcerts,
-    userLocation,
-    artistWishlist,
-    setArtistWishlist,
-    setConcerts) => {
-    //generate an optimized route based on existing concerts saved and the artist wishlist, with consideration to user current location
-    //console.log(`generate optimized concert route for the current selected artist`);
-    //console.log(JSON.stringify(artistWishlist));
-    //filter out concerts that are not performed by the artists from the artist wish list
-    //allow concert with duplicate id at this stage, inform user of the dupcliate at the end
-    var initialFilteredConcerts = allConcerts.filter((concert) => {
-        return artistWishlist.some(artistWishlistItem => artistWishlistItem.WishlistArtistId === concert.artistId)
-    });
-    //group the concerts by artistId
-    var groupedByArtistConcertList = initialFilteredConcerts.reduce((r, a) => {
-        r[a.artistId] = r[a.artistId] || [];
-        r[a.artistId].push(a);
-        return r;
-    }, Object.create(null));
+  allConcerts,
+  userLocation,
+  artistWishlist,
+  setArtistWishlist,
+  setConcerts) => {
+  //generate an optimized route based on existing concerts saved and the artist wishlist, with consideration to user current location
+  //console.log(`generate optimized concert route for the current selected artist`);
+  //console.log(JSON.stringify(artistWishlist));
+  //filter out concerts that are not performed by the artists from the artist wish list
+  //allow concert with duplicate id at this stage, inform user of the dupcliate at the end
+  var initialFilteredConcerts = allConcerts.filter((concert) => {
+    return artistWishlist.some(artistWishlistItem => artistWishlistItem.WishlistArtistId === concert.artistId)
+  });
+  //group the concerts by artistId
+  var groupedByArtistConcertList = initialFilteredConcerts.reduce((r, a) => {
+    r[a.artistId] = r[a.artistId] || [];
+    r[a.artistId].push(a);
+    return r;
+  }, Object.create(null));
 
-    //this should be a dictionary
-    //console.log(`groupedbyartistconcertlist: ${JSON.stringify(groupedByArtistConcertList)}`);
+  //this should be a dictionary
+  //console.log(`groupedbyartistconcertlist: ${JSON.stringify(groupedByArtistConcertList)}`);
 
-    // figure out all the possible combination of the shows 
-    // thanks stackoverflow, I'm not writing up this shit.
-    // https://stackoverflow.com/questions/4331092/finding-all-combinations-cartesian-product-of-javascript-array-values
-    // this needs to be a list of list
-    var allCombinationOfConcerts = generateCombinations(groupedByArtistConcertList);
+  // figure out all the possible combination of the shows 
+  // thanks stackoverflow, I'm not writing up this shit.
+  // https://stackoverflow.com/questions/4331092/finding-all-combinations-cartesian-product-of-javascript-array-values
+  // this needs to be a list of list
+  var allCombinationOfConcerts = generateCombinations(groupedByArtistConcertList);
 
-    if (allCombinationOfConcerts.length < 1) {
-        alert(`Unable to schedule a plan for the artists you want to see, their schedules conflict with each other`);
-        //remove the last attempted artist that was causing problem and run the optimization again
-        setArtistWishlist(artistWishlist.slice(0, -1));
-        GenerateOptimizedConcertRoute(allConcerts, userLocation, artistWishlist.slice(0, -1));
-    }
+  if (allCombinationOfConcerts.length < 1) {
+    alert(`Unable to schedule a plan for the artists you want to see, their schedules conflict with each other`);
+    //remove the last attempted artist that was causing problem and run the optimization again
+    setArtistWishlist(artistWishlist.slice(0, -1));
+    GenerateOptimizedConcertRoute(allConcerts, userLocation, artistWishlist.slice(0, -1));
+  }
 
-    //now sort the remaining by max distance traveled
-    allCombinationOfConcerts = allCombinationOfConcerts.sort((a, b) => {
-        return getTotalDistance(a, userLocation) - getTotalDistance(b, userLocation);
-    });
+  //now sort the remaining by max distance traveled
+  allCombinationOfConcerts = allCombinationOfConcerts.sort((a, b) => {
+    return getTotalDistance(a, userLocation) - getTotalDistance(b, userLocation);
+  });
 
-    //pick the route with the shortest distance
-    // this need to be a list
-    console.log(`allCombinationOfConcerts[0]: ${JSON.stringify(allCombinationOfConcerts[0])}`);
+  //pick the route with the shortest distance
+  // this need to be a list
+  console.table(`allCombinationOfConcerts[0]: ${allCombinationOfConcerts[0]}`);
 
-    if (allCombinationOfConcerts.length > 1) {
-        var optimizedConcerts = allCombinationOfConcerts[0];
-        setConcerts(optimizedConcerts);
-    }
+  if (allCombinationOfConcerts.length > 1) {
+    var optimizedConcerts = allCombinationOfConcerts[0];
+    setConcerts(optimizedConcerts);
+  }
 }
